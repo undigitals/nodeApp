e076f7ee8a0647da1b0f848483ec490973367b62
Umid Negmatullayev <undigitals@gmail.com>
2018-06-26 18:44:51 +0900

    adding simple routing

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        res.write('Hello World');
+        res.end();
+    }
 
-server.on('connection', (socket)=>{
-    console.log("new connection!!!");
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
 });
+
 
 server.listen(3000);
 
