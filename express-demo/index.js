50392b14a10311209283f0f6f545aeb135c8f94c
Umid Negmatullayev <undigitals@gmail.com>
2018-06-29 19:58:08 +0900

    environment var setted

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Hello world!!!');
+    res.send('Hello world!!');
 });
 
 app.get('/api/courses', (req, res)=>{
     res.send([1,2,3]);
 });
 
-app.listen(3000, ()=> console.log('Listening on port 3000!'));
+// to set environment var on the command line : export PORT=5000   <- any num
+const port = process.env.PORT || 3000;
+app.listen(3000, ()=> console.log(`Listening on port ${port}`));
